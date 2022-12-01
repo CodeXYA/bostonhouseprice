f4afcd7bbaf01655f0dd37a449a71750ea618976
Ankith Y A <ankiyenadka@gmail.com>
2022-12-01 13:18:07 +0530

    Web Application

--- app.py
+++ app.py
@@ -1,9 +1,29 @@
-from flask import Flask
-from flask import request
+from flask import Flask, render_template
+from flask import request, jsonify
+import pickle
+import numpy as np
 
 app = Flask(__name__)
 
-@app.route("/", methods=['GET','POST'])
-def housing():
-    if request.method == 'POST':
+# Load the Model
+regmodel = pickle.load(open('regmodel.pkl','rb'))
+scaler = pickle.load(open('scaling.pkl','rb'))
+
+
+@app.route("/")
+def home():
+    return render_template('home.html')
+
+@app.route('/predict_api', methods=['POST'])
+def predict_api():
+    data = request.json['data']
+    print(data)
+    print(np.array(list(data.values())).reshape(1,-1))
+    new_data = scaler.transform(np.array(list(data.values())).reshape(1,-1))
+    output = regmodel.predict(new_data)
+    print(output[0])
+    return jsonify(output[0])
         
+
+if __name__ =="__main__":
+    app.run(debug=True)
